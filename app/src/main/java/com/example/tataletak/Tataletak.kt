29ce8e1d572e07a803eadef932e9198625285cdd
ZhilPro27 <zhilalkrisna@gmail.com>
2025-10-16 15:37:49 +0700

    Menambahkan box ke dua di dalam fungsi TataletakBoxColumnRow

--- app/src/main/java/com/example/tataletak/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/Tataletak.kt
@@ -129,5 +129,14 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             }
         }
         Spacer(modifier = Modifier.height(10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+
+        }
     }
 }
