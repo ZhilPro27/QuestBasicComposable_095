cd253edfe22b8a11692851151328675e9798be36
ZhilPro27 <zhilalkrisna@gmail.com>
2025-10-16 15:29:23 +0700

    Mengimport gambar ke project dan membuat variable gambar

--- app/src/main/java/com/example/tataletak/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/Tataletak.kt
@@ -99,5 +99,5 @@ fun TataletakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
-
+    val gambar = painterResource(id = R.drawable.icons8_game_controller_100)
 }
